685dffb2e927715e17651f51d3d7bcdb4949990b
Benito Lorenzo Santos <benitolsantos@gmail.com>
2018-03-15 17:52:05 +0000

    automatically backed up by learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,17 +1,20 @@
 def using_include(array, element)
  array.include?(element)
+ #checks if element is in array
 end
 
 def using_sort(array)
  array.sort
+ #sorts array. Strings alphabetically, Numbers by value.
 end
 
 def using_reverse(array)
-
+ array.reverse
+ #flips order of array
 end
 
 def using_first(array)
-
+ array.first
 end
 
 def using_last(array)
